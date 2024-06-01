c6c0fe75cb294ad47133d3acd6901ae5caace29c
KB <92556407+kulbriz233@users.noreply.github.com>
2024-06-01 13:16:22 +0000

    Almost done with the whole project

--- myProjectWork3/App.js
+++ myProjectWork3/App.js
@@ -11,12 +11,12 @@ const Stack = createStackNavigator();
 const categories = [
   { id: '1', title: 'Exercise', tasks: 12, image: require('./assets/Images/exercise1.png') },
   { id: '2', title: 'Study', tasks: 12, image: require('./assets/Images/study1.png') },
-  { id: '3', title: 'Code', tasks: 8, image: require('./assets/Images/code.png') },
-  { id: '4', title: 'Cook', tasks: 5, image: require('./assets/Images/cook.png') },
-  { id: '5', title: 'Cycling', tasks: 7, image: require('./assets/Images/cycling.png') },
-  { id: '6', title: 'Teaching Hours', tasks: 4, image: require('./assets/Images/teach.png') },
-  { id: '7', title: 'Travel & Tour', tasks: 3, image: require('./assets/Images/travel.png') },
-  { id: '8', title: 'Health Care', tasks: 6, image: require('./assets/Images/health.png') },
+  { id: '3', title: 'Code', tasks: 10, image: require('./assets/Images/code.png') },
+  { id: '4', title: 'Cook', tasks: 8, image: require('./assets/Images/cook.png') },
+  { id: '5', title: 'Cycling', tasks: 11, image: require('./assets/Images/cycling.png') },
+  { id: '6', title: 'Teaching Hours', tasks: 12, image: require('./assets/Images/teach.png') },
+  { id: '7', title: 'Travel & Tour', tasks: 8, image: require('./assets/Images/travel.png') },
+  { id: '8', title: 'Health Care', tasks: 7, image: require('./assets/Images/health.png') },
 ];
 
 const tasks = [
